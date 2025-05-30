61170025c4e487eafd6c8e1b6535ee0363d18b67
jnluis <jnluis@ua.pt>
2025-05-30 16:04:48 +0100

    Add React + ViteJS + NextUI + TailWind CSS. Add Structure to allow multiple pages

---
+++ wine_shop/src/LandingPage.jsx
@@ -0,0 +1,11 @@
+import {Button} from "@nextui-org/react"
+
+export default function LandingPage() {
+  return (
+    <>
+    <div className="flex items-center justify-center min-h-screen">
+      <Button className="text-green-500"> Hello NextUI!</Button>
+    </div>
+    </>
+  );
+}
